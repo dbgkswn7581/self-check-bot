6ad6b8a7f2c6c29e3671f7dcabba01f0fdd22007
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2021-03-14 14:41:04 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -352,6 +352,7 @@ async def on_message(ctx):
             inputpsd = driver.find_element_by_xpath('//*[@id="WriteInfoForm"]/table/tbody/tr/td/input')
             # inputpsd.click()
             inputpsd.send_keys('6213')
+            inputpsd.send_keys(Keys.ENTER)
         except Exception as ex:
             embed = discord.Embed(title = "Failed",
             description = "#비번 입력 칸", color = discord.Color.red()
@@ -360,15 +361,15 @@ async def on_message(ctx):
             await ctx.channel.send(ex)
 
         
-        try:
-            #비번 이후 확인 버튼
-            driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
-        except Exception as ex:
-            embed = discord.Embed(title = "Failed",
-            description = "#비번 이후 확인 버튼", color = discord.Color.red()
-            )
-            await ctx.channel.send(embed=embed)
-            await ctx.channel.send(ex)
+        # try:
+        #     #비번 이후 확인 버튼
+        #     driver.find_element_by_xpath('//*[@id="btnConfirm"]').click()
+        # except Exception as ex:
+        #     embed = discord.Embed(title = "Failed",
+        #     description = "#비번 이후 확인 버튼", color = discord.Color.red()
+        #     )
+        #     await ctx.channel.send(embed=embed)
+        #     await ctx.channel.send(ex)
 
 
         time.sleep(2)
