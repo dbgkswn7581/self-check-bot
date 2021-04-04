6758d08e41d812b4eb1b2512d1313efb0f2f06b9
dbgkswn7581 <72482222+dbgkswn7581@users.noreply.github.com>
2021-04-04 23:43:32 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,6 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-driver.quit()
 
 #에러 발생
 class makeError(Exception):
